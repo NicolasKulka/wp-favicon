bbb1836ea974e439dbe2ef20a9e988049e5cc4a8
Nicolas Kulka <nkulka@beapi.fr>
2015-01-26 16:54:22 +0100

    Initial commit

---
+++ faviconWP.php
@@ -0,0 +1,44 @@
+<?php
+/*
+  Plugin Name: Plateforme WP Digital - Favicon
+  Version: 1.0
+  Plugin URI: http://www.plateformewpdigital.fr
+  Description: Add favicon
+  Author: Kulka Nicolas
+  Author URI: http://www.plateformewpdigital.fr
+  Domain Path: languages
+  Network: false
+  Text Domain: pwd-favicon
+ */
+// don't load directly
+if ( !defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// Plugin constants
+define( 'PWD_FAVICON_VERSION', '1.0' );
+define( 'PWD_FAVICON_FOLDER', 'faviconWP' );
+
+define( 'PWD_FAVICON_URL', plugin_dir_url( __FILE__ ) );
+define( 'PWD_FAVICON_DIR', plugin_dir_path( __FILE__ ) );
+
+// Function for easy load files
+function _pwd_favicon_load_files( $dir, $files, $prefix = '' ) {
+	foreach ( $files as $file ) {
+		if ( is_file( $dir . $prefix . $file . ".php" ) ) {
+			require_once($dir . $prefix . $file . ".php");
+		}
+	}
+}
+
+// Plugin client classes
+_pwd_favicon_load_files( PWD_FAVICON_DIR . 'classes/', array('plugin') );
+
+add_action( 'plugins_loaded', 'init_pwd_faviconplugin' );
+function init_pwd_faviconplugin() {
+
+    load_plugin_textdomain( 'pwd-favicon', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+
+	// Load client
+	new PWD_FAVICON_Plugin();
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Favicon WP ===
+Contributors: nicolaskulka
+Author URI: http://www.plateformewpdigital.fr
+Plugin URL: http://www.plateformewpdigital.fr
+Requires at Least: 3.2
+Tested Up To: 4.1
+Tags: theme, favicon, admin, blog, wordpress, icon, iphone, apple touch, apple touch icon, upload
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows you to add upload custom favicon & Apple touch icon for your website and WordPress Dashboard using API customizer.
+
+== Description ==
+
+Now easily upload a favicon and apple touch icon for your WordPress website and dashboard.
+
+My Links =
+
+* Twitter @[KulkaNicolas](https://twitter.com/KulkaNicolas)
+* Google+ [Nicolas Kulka](https://plus.google.com/u/0/105181416749428983955/)
+
+If you love the plugin, please consider rating it and clicking on "it works" button.
+
+== Installation ==
+
+1. Upload the directory `/faviconWP/` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Click on "Customize" > "Favicon" and upload your favicon
+
+== Changelog ==
+
+= 1.0.0
+* This is the first version
