81365aadab51332a00128ecf4774f6596008c66c
Nicolas <kulka.nicolas@gmail.com>
2015-01-26 21:37:38 +0100

    add languages files

--- wp-favicon.php
+++ wp-favicon.php
@@ -1,14 +1,14 @@
 <?php
 /*
-  Plugin Name: Plateforme WP Digital - Favicon
+  Plugin Name: WP favicon
   Version: 1.0
-  Plugin URI: http://www.plateformewpdigital.fr
+  Plugin URI: http://www.plateformewpdigital.fr/plugins/wp-favicon
   Description: Add favicon
-  Author: Kulka Nicolas
+  Author: Plateforme WP Digital, Kulka Nicolas
   Author URI: http://www.plateformewpdigital.fr
   Domain Path: languages
   Network: false
-  Text Domain: pwd-favicon
+  Text Domain: wp-favicon
  */
 // don't load directly
 if ( !defined( 'ABSPATH' ) ) {
@@ -17,7 +17,7 @@ if ( !defined( 'ABSPATH' ) ) {
 
 // Plugin constants
 define( 'PWD_FAVICON_VERSION', '1.0' );
-define( 'PWD_FAVICON_FOLDER', 'faviconWP' );
+define( 'PWD_FAVICON_FOLDER', 'wp-favicon' );
 
 define( 'PWD_FAVICON_URL', plugin_dir_url( __FILE__ ) );
 define( 'PWD_FAVICON_DIR', plugin_dir_path( __FILE__ ) );
@@ -37,7 +37,7 @@ _pwd_favicon_load_files( PWD_FAVICON_DIR . 'classes/', array('plugin') );
 add_action( 'plugins_loaded', 'init_pwd_faviconplugin' );
 function init_pwd_faviconplugin() {
 
-    load_plugin_textdomain( 'pwd-favicon', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain( 'wp-favicon', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 
 	// Load client
 	new PWD_FAVICON_Plugin();
